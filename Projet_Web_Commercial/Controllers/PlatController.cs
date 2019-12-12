c2fc0588ce76382dd47843d079f73a953fc110f9
Soupiak <Soupiak@users.noreply.github.com>
2019-12-12 22:51:33 +0100

    Commit test

--- Projet_Web_Commercial/Controllers/PlatController.cs
+++ Projet_Web_Commercial/Controllers/PlatController.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Projet_Web_Commercial.Models;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Projet_Web_Commercial;
 
 namespace Projet_Web_Commercial.Controllers
 {
     public class PlatController : Controller
     {
-        const string chaineCo = @""
+        const string chaineConnexion = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Julian\source\repos\Projet_Web_Commercial\CommercialProject\Projet_Web_Commercial\Data\ProjetCommercialData.mdf;Integrated Security=True";
+
         public IActionResult Index()
         {
-            var liste = new List<Plat>()
+            try
             {
-                
-            };
-            return View(liste);
-        }
+                using (var connection = new SqlConnection(chaineConnexion))
+                {
+                    var liste = connection.Query<Plat>("SELECT * FROM Plat");
 
-        public IActionResult Details(int id)
-        {
-            return View();
+                    return View(liste);
+                }
+            } // Appel du Dispose de connection (ferme la connexion)
+            catch (SqlException e)
+            {
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
---
+++ Projet_Web_Commercial/Models/PlatEntite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projet_Web_Commercial.Models
+{
+    public class PlatEntite
+    {
+        public int Id { get; set; } // Propriété automatique
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Nom { get; set; }
+
+        public int Prix { get; set; }
+        public int Poid { get; set; }
+
+    }
+}
